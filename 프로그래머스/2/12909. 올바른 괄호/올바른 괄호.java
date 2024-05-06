3ec346db48cb11a265310d65cf0f5477315b5412
Jang-GO <88973153+Jang-GO@users.noreply.github.com>
2024-05-06 15:36:12 +0900

    [level 2] Title: 올바른 괄호, Time: 17.48 ms, Memory: 52.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> st = new Stack<>();
+        if(s.charAt(0)==')') return false;
+        
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='(') st.push(s.charAt(i));
+            else{
+                if(!st.isEmpty()) st.pop();
+            }
+        }
+        
+        return (st.size()==0) ? true : false;
+    }
+}

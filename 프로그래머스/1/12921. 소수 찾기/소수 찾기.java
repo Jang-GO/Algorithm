ff4e1497677a28e683b4e788f1a21ff21ccc6859
Jang-GO <88973153+Jang-GO@users.noreply.github.com>
2024-05-19 05:00:51 +0900

    [level 1] Title: 소수 찾기, Time: 17.97 ms, Memory: 55.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        if(n==1) return 0;
+        int[] arr = new int[n+1];
+        int cnt =0;
+        for(int i=2;i<=n;i++){
+            if(arr[i]==0){
+                cnt++;
+                for(int j=i;j<=n;j+=i) arr[j]=1;
+            }
+        }
+        return cnt;
+    }
+}

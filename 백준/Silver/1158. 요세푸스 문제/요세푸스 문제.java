ade0c02c9752142ff735eab1557a73ea3e59c9da
Jang-GO <88973153+Jang-GO@users.noreply.github.com>
2025-02-02 13:27:32 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 584 ms, Memory: 294912 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -0,0 +1,55 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        Main T = new Main();
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] tmp = br.readLine().split(" ");
+        int n = Integer.parseInt(tmp[0]);
+        int k = Integer.parseInt(tmp[1]);
+
+        int[] answer = T.solution(n, k);
+        printAnswer(sb, answer);
+    }
+
+    private int[] solution(int n, int k) {
+        List<Integer> answer = new ArrayList<>();
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=1;i<=n;i++){
+            q.offer(i);
+        }
+
+        int cnt = 0;
+        while(!q.isEmpty()){
+            cnt++;
+            if(cnt==k){
+                answer.add(q.poll());
+                cnt = 0;
+            }else{
+                q.offer(q.poll());
+            }
+        }
+
+        return answer.stream().mapToInt(x -> x).toArray();
+    }
+
+    private static void printAnswer(StringBuilder sb, int[] answer) {
+        sb.append("<");
+        for(int a: answer){
+            sb.append(a);
+            sb.append(", ");
+        }
+        sb.delete(sb.length()-2, sb.length());
+        sb.append(">");
+        System.out.println(sb);
+    }
+}

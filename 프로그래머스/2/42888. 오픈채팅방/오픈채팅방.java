702926362fa53790bda91fa5e6daef1c24939bda
Jang-GO <88973153+Jang-GO@users.noreply.github.com>
2025-02-11 14:13:27 +0900

    [level 2] Title: 오픈채팅방, Time: 200.16 ms, Memory: 165 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -1,29 +1,61 @@
 import java.util.*;
 
 class Solution {
-    public List<String >solution(String[] record) {
-        List<String> answer = new ArrayList<>();
-        Map<String, String> map = new HashMap<>();
+    // 리팩토링
+    // 1. foreach -> for with index
+    // 2. StringTokenier, StringBuilder
+    // 3. msg HashMap 제거
+    // 4. 무조건 split() -> 선택적 처리
+    public String[] solution(String[] record) {
+        HashMap<String, String> uid = new HashMap<>();
         
-        for(String str: record){
-            String[] strArr = str.split(" ");
-            String command = strArr[0];
-            if(command.equals("Enter") || command.equals("Change")){
-                map.put(strArr[1], strArr[2]);    
-            }
+        for(int i=0;i<record.length;i++){
+            char first = record[i].charAt(0);
+            if(first == 'E' || first == 'C'){
+                StringTokenizer st = new StringTokenizer(record[i]);
+                st.nextToken(); // 첫번쨰껀 버림
+                uid.put(st.nextToken(), st.nextToken());
+            } 
         }
         
-        for (String str : record) {
-            String[] strArr = str.split(" ");
-            String command = strArr[0];
-            
-            if (command.equals("Enter")) {
-                answer.add(map.get(strArr[1]) + "님이 들어왔습니다.");
-            } else if (command.equals("Leave")) {
-                answer.add(map.get(strArr[1]) + "님이 나갔습니다.");
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=0;i<record.length;i++){
+            char first = record[i].charAt(0);
+            if(first == 'E' || first == 'L'){
+                StringTokenizer st = new StringTokenizer(record[i]);
+                st.nextToken();
+                sb.append(uid.get(st.nextToken())).append( first == 'E' ? "님이 들어왔습니다.,":"님이 나갔습니다.,");
             }
         }
+         sb.setLength(sb.length() -1); // 전체 문자열 마지막 콤마 제거
         
-        return answer;
+        return sb.toString().split(",");
     }
+    
+//     public List<String >solution(String[] record) {
+//         List<String> answer = new ArrayList<>();
+//         Map<String, String> map = new HashMap<>();
+        
+//         for(String str: record){
+//             String[] strArr = str.split(" ");
+//             String command = strArr[0];
+//             if(command.equals("Enter") || command.equals("Change")){
+//                 map.put(strArr[1], strArr[2]);    
+//             }
+//         }
+        
+//         for (String str : record) {
+//             String[] strArr = str.split(" ");
+//             String command = strArr[0];
+            
+//             if (command.equals("Enter")) {
+//                 answer.add(map.get(strArr[1]) + "님이 들어왔습니다.");
+//             } else if (command.equals("Leave")) {
+//                 answer.add(map.get(strArr[1]) + "님이 나갔습니다.");
+//             }
+//         }
+        
+//         return answer;
+//     }
 }

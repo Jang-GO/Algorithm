03623438b0b097f4dfb3530e44731d5c0ce07c7e
Jang-GO <88973153+Jang-GO@users.noreply.github.com>
2025-02-17 15:51:38 +0900

    [Silver II] Title: 투에-모스 문자열, Time: 104 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Silver/18222. 투에－모스 문자열/투에－모스 문자열.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long k = Long.parseLong(br.readLine());
+        long size = 1;
+
+        // k 번째 수를 구할 수 있을 때까지만 size를 증가
+        while (size < k) {
+            size *= 2;
+        }
+
+        System.out.println(solution(k, size));
+    }
+
+    private static int solution(long k, long size) {
+        if (size == 1) return 0;
+
+        long mid = size / 2;
+
+        if (k > mid) {
+            k -= mid;
+            return (solution(k, mid) == 0) ? 1 : 0;
+        } else return solution(k, mid);
+    }
+}

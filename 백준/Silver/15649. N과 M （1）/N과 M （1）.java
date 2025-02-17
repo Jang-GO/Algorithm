d327f346880f6b28e0f530b8cac2ff77bea2c6b6
Jang-GO <88973153+Jang-GO@users.noreply.github.com>
2025-02-17 17:10:38 +0900

    [Silver III] Title: N과 M (1), Time: 608 ms, Memory: 42748 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,12 +1,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.stream.IntStream;
 
 public class Main {
-    static int n,m;
+    static int n, m;
     static int[] arr;
-    static boolean ch[];
+    static boolean[] ch;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,24 +16,27 @@ public class Main {
         arr = new int[m];
         ch = new boolean[n];
 
-        perm(0);
+        perm(0, 0);
     }
 
-    private static void perm(int L) {
-        if(L==m){
+    private static void perm(int L, int count) {
+        // 가지치기: 남은 숫자의 개수가 부족하면 탐색 중단
+        if (n - count < m - L) return;
+
+        if (L == m) {
             StringBuilder sb = new StringBuilder();
-            for(int i=0;i<m;i++) sb.append(arr[i] + " ");
+            for (int i = 0; i < m; i++) sb.append(arr[i]).append(" ");
             System.out.println(sb);
-        }else{
-            for(int i=0;i<n;i++){
-                if(!ch[i]){
-                    ch[i] = true;
-                    arr[L] = i+1;
-                    perm(L+1);
-                    ch[i] = false;
-                }
-            }
+            return;
         }
 
+        for (int i = 0; i < n; i++) {
+            if (!ch[i]) {
+                ch[i] = true;
+                arr[L] = i + 1;
+                perm(L + 1, count + 1);
+                ch[i] = false;
+            }
+        }
     }
 }

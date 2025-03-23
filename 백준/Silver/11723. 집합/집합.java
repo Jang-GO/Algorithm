12b9a42ae621f6021f82f49b209c1fe2bc293201
Jang-GO <88973153+Jang-GO@users.noreply.github.com>
2025-03-23 22:29:25 +0900

    [Silver V] Title: 집합, Time: 1152 ms, Memory: 307304 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,44 +1,40 @@
 import java.io.*;
-import java.util.*;
 
-class Main{
-    static StringBuilder sb = new StringBuilder();
-    static Set<Integer> set = new HashSet<>();
-    public static void main(String[] args) throws IOException{
+public class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int m = Integer.parseInt(br.readLine());
-        for(int i=0;i<m;i++){
-            String[] tmp = br.readLine().split(" ");
-            String a;
-            int b=-1;
-            if(tmp.length==1){
-                a = tmp[0];
-            }else{
-                a = tmp[0];
-                b = Integer.parseInt(tmp[1]);
-            }
-            if(a.equals("check")){
-                sb.append(set.contains(b) ? "1" : "0").append("\n");
-            }else if(a.equals("add")){
-                if(set.contains(b)) continue;
-                else set.add(b);
-            }else if(a.equals("remove")){
-                if(!set.contains(b)) continue;
-                else set.remove(b);
-            }else if(a.equals("toggle")){
-                if(set.contains(b)) set.remove(b);
-                else set.add(b);
-            }else if(tmp.length==1){
-                if(a.equals("all")){
-                    for(int j=1;j<=20;j++){
-                        set.add(j);
-                    }
-                }else{
-                    set.clear();
+        StringBuilder sb = new StringBuilder();
+        int S = 0; // 비트마스킹을 활용한 집합
+
+        int M = Integer.parseInt(br.readLine()); // 연산 개수
+
+        while (M-- > 0) {
+            String[] command = br.readLine().split(" ");
+            String op = command[0];
+
+            if (op.equals("all")) {
+                S = (1 << 20) - 1;
+            } else if (op.equals("empty")) {
+                S = 0;
+            } else {
+                int x = Integer.parseInt(command[1]) - 1; 
+                switch (op) {
+                    case "add":
+                        S |= (1 << x);
+                        break;
+                    case "remove":
+                        S &= ~(1 << x);
+                        break;
+                    case "check":
+                        sb.append((S & (1 << x)) != 0 ? "1\n" : "0\n");
+                        break;
+                    case "toggle":
+                        S ^= (1 << x);
+                        break;
                 }
             }
         }
 
-        System.out.println(sb.toString());
+        System.out.print(sb);
     }
 }

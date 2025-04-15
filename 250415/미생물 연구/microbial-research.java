e3febbb41d64e2ec52077a4d745d8cd52ad47b42
Jang-GO <88973153+Jang-GO@users.noreply.github.com>
2025-04-15 15:16:31 +0900

    Title: 미생물 연구, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250415/미생물 연구/microbial-research.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    static int[][] arr;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        
+    }
+}

031dcc72b9324fe4e0c87a66f76107d72871f889
Jang-GO <88973153+Jang-GO@users.noreply.github.com>
2025-11-12 22:29:49 +0900

    [Silver II] Title: 유기농 배추, Time: 124 ms, Memory: 15748 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,64 +1,61 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
-class Main{
-    static int n,m;
-    static int[] dx = {1,0,-1,0};
-    static int[] dy = {0,-1,0,1};
-    static class Point{
-        int x;
-        int y;
-        Point(int x,int y){
-            this.x = x;
-            this.y = y;
+public class Main {
+    static int[] dy = {1,0,-1,0};
+    static int[] dx = {0,1,0,-1};
+
+    private static void DFS(int y, int x, int[][] arr){
+        arr[y][x] = 0;
+
+        for(int i=0;i<4;i++){
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            if(ny >= 0 && ny < arr.length && nx >= 0 && nx < arr[0].length && arr[ny][nx]==1){
+                DFS(ny,nx,arr);
+            }
         }
     }
-    public static void BFS(int[][] board,int x, int y){
-        Queue<Point> Q = new LinkedList<>();
-        Q.offer(new Point(x,y));
-        board[x][y]=0;
-        while(!Q.isEmpty()){
-            Point tmp = Q.poll();
-            for(int i=0;i<4;i++){
-                int nx = tmp.x + dx[i];
-                int ny = tmp.y + dy[i];
-                if(nx>=0 && nx < n && ny>=0 && ny<m && board[nx][ny]==1){
-                    board[nx][ny]=0;
-                    Q.offer(new Point(nx, ny));
+
+    private static int solution(int[][] arr){
+        int answer = 0;
+
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                if(arr[i][j]==1){
+                    DFS(i, j, arr);
+                    answer++;
                 }
             }
         }
+
+        return answer;
     }
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        for(int i=0;i<t;i++){
-            int cnt=0;
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for(int t=0;t<T;t++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-            m = Integer.parseInt(st.nextToken());
-            n = Integer.parseInt(st.nextToken());
-            int k = Integer.parseInt(st.nextToken());
-            int[][] board = new int[n][m];
-            for(int j=0;j<k;j++){
-                StringTokenizer st1 = new StringTokenizer(br.readLine());
-                int x = Integer.parseInt(st1.nextToken());
-                int y = Integer.parseInt(st1.nextToken());
-                board[y][x] = 1;
+            int M = Integer.parseInt(st.nextToken());
+            int N = Integer.parseInt(st.nextToken());
+            int K = Integer.parseInt(st.nextToken());
+
+            int[][] arr = new int[N][M];
+
+            for(int i=0;i<K;i++){
+                st = new StringTokenizer(br.readLine());
+                int X = Integer.parseInt(st.nextToken());
+                int Y = Integer.parseInt(st.nextToken());
+                arr[Y][X] = 1;
             }
-            
-            for(int j=0;j<n;j++){
-                for(int l=0;l<m;l++){
-                    if(board[j][l]==1){
-                        BFS(board,j,l);
-                        cnt++;
-                    }
-                }  
-            }
-            System.out.println(cnt);
+
+            sb.append(solution(arr)).append("\n");
         }
+
+        System.out.println(sb);
     }
 }

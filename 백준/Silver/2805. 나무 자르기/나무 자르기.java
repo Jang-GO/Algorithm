6fe1e6c9b115f96d963e64fc7014f0a1866b5cd5
Jang-GO <88973153+Jang-GO@users.noreply.github.com>
2026-01-08 09:45:21 +0900

    [Silver II] Title: 나무 자르기, Time: 692 ms, Memory: 154028 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,54 +1,44 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-    
-    public long checkHeight(int[] arr, int mid) {
-        long sum = 0;
-        for (int x : arr) {
-            if (x > mid) sum += (x - mid);
-        }
-        return sum;
-    }
-
-    public int solution(int n, int m, int[] trees) {
-        Arrays.sort(trees);
+class Main{
+    private static int solution(int n, int m, int[] arr){
+        Arrays.sort(arr);
         int answer = 0;
         int lt = 0;
-        int rt = trees[n - 1];
-
-        while (lt <= rt) {
-            int mid = (lt + rt) >>> 1; 
-            long wood = checkHeight(trees, mid); 
-            
-            if (wood >= m) {
-                answer = mid; 
-                lt = mid + 1;
-            } else {
-                rt = mid - 1;
+        int rt = arr[n-1];
+        
+        while(lt<=rt){
+            int mid = (lt+rt)/2;
+            if(cut(mid, arr) >= m){
+                answer = mid;
+                lt = mid+1;
+            }else{
+                rt = mid-1;
             }
         }
         
         return answer;
     }
-
-    public static void main(String[] args) throws IOException {
-        Main T = new Main();
+    
+    private static long cut(int mid, int[] arr){
+        long sum = 0;
+        for(int target : arr) if(target > mid) sum+=(target-mid);
+        return sum;
+    }
+    
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
         StringTokenizer st = new StringTokenizer(br.readLine());
+        
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        int[] trees = new int[n];
         
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; st.hasMoreTokens(); i++) {
-            trees[i] = Integer.parseInt(st.nextToken());
-        }
+        int[] arr = new int[n];
         
-        System.out.println(T.solution(n, m, trees));
+        for(int i=0;st.hasMoreTokens();i++) arr[i] = Integer.parseInt(st.nextToken());
+        
+        System.out.println(solution(n,m,arr));
     }
 }
